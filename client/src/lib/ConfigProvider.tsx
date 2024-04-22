cb3c6ebe260a157b33ba44034b80b601fdbe5b2c
Enes Çelik <enescelik6012@gmail.com>
2024-04-22 11:24:06 +0300

    Update dependencies and add redux-persist to package.json

--- client/src/lib/ConfigProvider.tsx
+++ client/src/lib/ConfigProvider.tsx
@@ -1,8 +1,9 @@
 "use client";
-import { useAppSelector } from "@/store/store";
-import { ConfigProvider, theme } from "antd";
+import { ConfigProvider, Spin, theme } from "antd";
 import trTR from "antd/locale/tr_TR";
 import "dayjs/locale/tr";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "@/store/store";
 
 const ConfigProviderComp = ({ children }: { children: React.ReactNode }) => {
   const { darkAlgorithm } = theme;
@@ -13,7 +14,18 @@ const ConfigProviderComp = ({ children }: { children: React.ReactNode }) => {
         token: {},
       }}
     >
-      {children}
+      <PersistGate
+        loading={
+          <>
+            <div className="w-full h-[100vh] flex items-center justify-center">
+              <Spin size="large" />
+            </div>
+          </>
+        }
+        persistor={persistor}
+      >
+        {children}
+      </PersistGate>
     </ConfigProvider>
   );
 };

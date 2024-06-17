edcfd88ddd0155d3da3c02510ecf5a58348fd1ad
Enes Çelik <enescelik6012@gmail.com>
2024-06-17 22:45:30 +0300

    feat: Update fetchCustomer action to handle error response

--- client/src/actions/fetchCustomer.ts
+++ client/src/actions/fetchCustomer.ts
@@ -23,7 +23,6 @@ export const fetchCustomer = async () => {
         cache: "no-cache",
       }
     );
-
     if (!response.ok) {
       const message = await response.json();
       throw new Error(message.errorMessage);
--- client/src/components/Customer/CustomerContainer.tsx
+++ client/src/components/Customer/CustomerContainer.tsx
@@ -3,12 +3,17 @@ import React from "react";
 import CustomerContainerHeader from "./CustomerContainerHeader";
 import CustomerTable from "./CustomerTable/CustomerTable";
 import { fetchCustomer } from "@/actions/fetchCustomer";
+import ErrorPage from "../ErrorPage";
 
 const CustomerContainer = async () => {
   const { data, error } = await fetchCustomer();
 
+  if (error) {
+    return <ErrorPage error={error} status="500" title="Error" key={error} />;
+  }
+
   return (
-    <Card className="overflow-auto flex flex-col gap-2">
+    <Card className="overflow-auto flex flex-col gap-2 ">
       <CustomerContainerHeader />
       <CustomerTable customers={data} />
     </Card>
